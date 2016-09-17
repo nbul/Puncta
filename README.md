85790fd3bebede3da7b4989cc9dd653795d451f9
nbul <nbul001@gmail.com>
2016-09-17 13:42:01 +0100

    v1
    This is the first version of the script. Modified and re-worked from
    Strutt’s lab.

---
+++ README.md
@@ -0,0 +1,14 @@
+# MAS
+Script to analyse intensity and area of muscle attachment sites
+
+Files should be labelled by consequent numbers starting with 1 (i.e. 1,2,3,4...)
+
+Output file All.csv contains information about every individual
+attachment site, whereas Average.csv contains avarage values per embryo.
+The output files will be saved in the same folder as image files.
+
+Embryos should be imaged being aligned along X-axis (with respect to 
+anterior-posterior axis)
+
+The script should work on any type of images, but I have tested only on
+Olympus Upright .oib files.
---
+++ control.m
@@ -0,0 +1,226 @@
+%% Processing control files and getting averages
+%% Memory allocation
+Parea_av = zeros(numel(control_files),1);
+Pareastd_av = zeros(numel(control_files),1);
+Pmean_av = zeros(numel(control_files),1);
+Pmeanstd_av = zeros(numel(control_files),1);
+Pmax_av = zeros(numel(control_files),1);
+Pmaxstd_av = zeros(numel(control_files),1);
+Pmin_av = zeros(numel(control_files),1);
+Pminstd_av = zeros(numel(control_files),1);
+Pareatotal_av = zeros(numel(control_files),1);
+Pn = zeros(numel(control_files),1);
+
+Carea_av = zeros(numel(control_files),1);
+Careastd_av = zeros(numel(control_files),1);
+Cmean_av = zeros(numel(control_files),1);
+Cmeanstd_av = zeros(numel(control_files),1);
+Cmax_av = zeros(numel(control_files),1);
+Cmaxstd_av = zeros(numel(control_files),1);
+Cmin_av = zeros(numel(control_files),1);
+Cminstd_av = zeros(numel(control_files),1);
+Cn = zeros(numel(control_files),1);
+
+Marea_av = zeros(numel(control_files),1);
+Mareastd_av = zeros(numel(control_files),1);
+Mmean_av = zeros(numel(control_files),1);
+Mmeanstd_av = zeros(numel(control_files),1);
+Mmax_av = zeros(numel(control_files),1);
+Mmaxstd_av = zeros(numel(control_files),1);
+Mmin_av = zeros(numel(control_files),1);
+Mminstd_av = zeros(numel(control_files),1);
+Mn = zeros(numel(control_files),1);
+tolerance_achieved = zeros(numel(control_files),1);
+Mmean_nonpuncta = zeros(numel(control_files),1);
+
+relative_puncta_area2 = zeros(numel(control_files),1);
+
+%% Processing control files and getting average
+for i=1:numel(control_files)
+    cd(tif16_dir_control);
+    filename = [num2str(i), fileext];
+    imdata = bfopen(filename);
+    
+    % Image and filepath is in first cell
+    imdata = imdata{1};
+    % Image is in first array
+    originalimage = imdata{1};
+    
+    %crop edges since packing analyzer leaves a white border on the mask
+    %note: I2=imcrop(I,rect) crops image I, rect is position vector [xmin,
+    %ymin, width, height] which defines the crop area
+    
+    [image_height, image_width, ~] = size(originalimage);
+    rect = [2,2,image_width-4, image_height-4];
+    originalimage = imcrop(originalimage, rect);
+    
+    %add a black border of size 'border' to image
+    border = 10;
+    originalimage = padarray(originalimage,[border, border]);
+    
+    % 2) Gentle Gaussian smoothing to even out the noise:
+    filteredimage = imfilter(originalimage, fspecial('Gaussian', 5, 0.75));
+    % 3) Median filter to further reduce noise while preserving edges:
+    filteredimage = medfilt2(filteredimage, [5 5]);
+    
+    % *** Apply membrane mask, if specified, otherwise mask just "black" regions ***
+    
+    if  strcmp(usemask, 'Yes')
+        
+        % Specify path for Packing Analyzer hand-corrected membrane mask.
+        maskpath = [tif8_dir_control, '/', num2str(i), '/handCorrection.png'];
+        
+        % Need open membrane skeleton file and process before use
+        skel = imread(maskpath);
+        % dilate, erode to clean up. "radius" is half final thickness
+        skel = rgb2gray(skel);
+        skel = logical(skel);
+        skel = imcrop(skel, rect); %crop edges of mask file
+        skel = padarray(skel,[border, border]); %add border to mask fileskel = imclearborder(skel);
+        skel = imclearborder(skel);
+        skel = imdilate(skel, strel('disk', maskradius, 0));
+        skel = bwmorph(skel, 'diag');
+        skel = bwmorph(skel, 'thin', Inf);
+        skel = bwmorph(skel, 'spur', Inf);
+        % final dilation to create membrane mask
+        mask = imdilate(skel, strel('disk', maskradius, 0));
+        % mask filtered image, n.b. converts to binary
+        unmasked_regions = filteredimage & mask;
+        
+    else
+        
+        % mask off any completely black regions (assuming image has been cropped)
+        unmasked_regions = im2bw(filteredimage,0);
+        
+    end
+    binaryimage = im2bw(filteredimage, thresh_new);
+    binaryimage = binaryimage & unmasked_regions;
+    
+    % Next lines tidy up remaining puncta map and remove objects below minobjectsize
+    binaryimage = bwmorph(binaryimage, 'bridge');
+    binaryimage = bwmorph(binaryimage, 'clean');
+    binaryimage = imfill(binaryimage, 'holes');
+    binaryimage = bwmorph(binaryimage, 'close');
+    binaryimage = imfill(binaryimage, 'holes');
+    binaryimage = bwareaopen(binaryimage, minobjectsize);
+    
+    % *** Get Puncta Stats ***
+    
+    if  strcmp(usemask, 'Yes')
+        
+        % Create inverse cytoplasmic mask
+        cyto = imcomplement(unmasked_regions);
+        cyto = imclearborder(cyto);
+        
+        % Get Puncta stats
+        
+        PUNCTA = bwconncomp(binaryimage);
+        
+        Pdata = regionprops(PUNCTA, originalimage, 'Area', 'MeanIntensity','MaxIntensity', 'MinIntensity');
+        Parea = [Pdata.Area];
+        Pmean = [Pdata.MeanIntensity];
+        Pmax = [Pdata.MaxIntensity];
+        Pmin = [Pdata.MinIntensity];
+        
+        Parea_av(i) = pixelarea * mean(Parea);
+        Pareastd_av(i) = std(im2double(Parea));
+        Pareatotal_av(i) = pixelarea * sum(Parea);
+        Pmeanstd_av(i) = std(Pmean);
+        Pmean_av(i) = mean(Pmean);
+        Pmaxstd_av(i) = std(im2double(Pmax));
+        Pmax_av(i) = mean(Pmax);
+        Pminstd_av(i) = std(im2double(Pmin));
+        Pmin_av(i) = mean(Pmin);
+        
+        Pn(i) = PUNCTA.NumObjects;
+        
+        % *** Cytoplasm stats: ***
+        
+        CYTO = bwconncomp(cyto);
+        
+        Cdata = regionprops(CYTO, originalimage, 'Area', 'MeanIntensity','MaxIntensity', 'MinIntensity');
+        Carea = [Cdata.Area];
+        Cmean = [Cdata.MeanIntensity];
+        Cmax = [Cdata.MaxIntensity];
+        Cmin = [Cdata.MinIntensity];
+        
+        Carea_av(i) = pixelarea * mean(Carea);
+        Careastd_av(i) = std(im2double(Carea));
+        Cmeanstd_av(i) = std(Cmean);
+        Cmean_av(i) = mean(Cmean);
+        Cmaxstd_av(i) = std(im2double(Cmax));
+        Cmax_av(i) = mean(Cmax);
+        Cminstd_av(i) = std(im2double(Cmin));
+        Cmin_av(i) = mean(Cmin);
+        
+        Cn(i) = CYTO.NumObjects;
+        
+        % *** Membrane stats ***
+        % Membrane is a single fused object.
+        
+        % Create mask for total area of cells and membrane, based on Packing
+        % Analyzer skeleton (ensures outer cells only include half shared membrane)
+        % membrane = imclearborder(~skel, 4);
+        
+        % Substract cytoplasm to give final membrane mask
+        % membrane = membrane - cyto;
+        
+        MEMBRANE = bwconncomp(unmasked_regions);
+        
+        Mdata = regionprops(MEMBRANE, originalimage, 'Area', 'MeanIntensity','MaxIntensity', 'MinIntensity');
+        Marea = [Mdata.Area];
+        Mmean = [Mdata.MeanIntensity];
+        Mmax = [Mdata.MaxIntensity];
+        Mmin = [Mdata.MinIntensity];
+        
+        Marea_av(i) = pixelarea * mean(Marea);
+        Mareastd_av(i) = std(im2double(Marea));
+        Mmeanstd_av(i) = std(Mmean);
+        Mmean_av(i) = mean(Mmean);
+        Mmaxstd_av(i) = std(im2double(Mmax));
+        Mmax_av(i) = mean(Mmax);
+        Mminstd_av(i) = std(im2double(Mmin));
+        Mmin_av(i) = mean(Mmin);
+        
+        Mn(i) = MEMBRANE.NumObjects;
+        
+        Mmean_nonpuncta(i) = ((Marea_av(i) * Mmean_av(i)) - (Pareatotal_av(i) * Pmean_av(i)))/(Marea_av(i)-Pareatotal_av(i));
+        
+    else
+        
+        % Get Puncta stats
+        
+        PUNCTA = bwconncomp(binaryimage);
+        
+        Pdata = regionprops(PUNCTA, originalimage, 'Area', 'MeanIntensity','MaxIntensity', 'MinIntensity');
+        Parea = [Pdata.Area];
+        Pmean = [Pdata.MeanIntensity];
+        Pmax = [Pdata.MaxIntensity];
+        Pmin = [Pdata.MinIntensity];
+        
+        Parea_av(i) = pixelarea * mean(Parea);
+        Pareastd_av(i) = std(im2double(Parea));
+        Pareatotal_av(i) = pixelarea * sum(Parea);
+        Pmeanstd_av(i) = std(Pmean);
+        Pmean_av(i) = mean(Pmean);
+        Pmaxstd_av(i) = std(im2double(Pmax));
+        Pmax_av(i) = mean(Pmax);
+        Pminstd_av(i) = std(im2double(Pmin));
+        Pmin_av(i) = mean(Pmin);
+        
+        Pn(i) = PUNCTA.NumObjects;
+        
+    end
+    
+    % *** Add stats to output array, including filename and data on threshold ***
+    relative_puncta_area2(i) = Pareatotal/unmasked_total(i);
+    tolerance_achieved(i) = abs(target_area - relative_puncta_area2(i));
+    
+    cd(im1_dir);
+    imwrite(binaryimage, [num2str(i), '_binary.tif'], 'tif');
+    cd(im2_dir);
+    % *** Save mask used to image folder ***
+    imwrite(unmasked_regions,[num2str(i), '_mask.tif'], 'tif');
+
+    
+end
